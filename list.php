964acdfdd4fbc71412aa0db534aaadcd27c28377
arkaddev <105284547+arkaddev@users.noreply.github.com>
2025-02-13 23:16:45 +0100

    list.php was created

---
+++ list.php
@@ -0,0 +1,22 @@
+<?php
+require 'config/session.php';
+require 'config/db_connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Witaj</title>
+
+ 
+</head>
+<body>
+  
+  
+  
+  
+
+</body>
+</html>
