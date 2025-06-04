36b9ee0ead2f8993ef37e3a58e739f1c46db76be
arkaddev <105284547+arkaddev@users.noreply.github.com>
2025-06-04 23:24:22 +0200

    function update_service was created in add_part.php

--- requests/list/add_part.php
+++ requests/list/add_part.php
@@ -31,7 +31,65 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['part_id_service']))  
     } else {
         echo json_encode(['status' => 'error', 'message' => 'Błąd podczas aktualizacji danych']);
     }
+  
+  update_service();
+  
     exit();
+}
+
+// funkcja wykonuje sie klicknieciu w przycisk ktory potwierdza wymienienie czesci
+// sprawdzenie czy są jakies czesci do wymiany, jezeli nie ma to ustawienie service_flag na 0
+function update_service(){
+    global $conn;
+
+    $car_id = intval($_POST['car_id']);
+    $query = "
+        SELECT
+            parts.id,
+            parts.car_id, 
+            parts.name, 
+            parts.number, 
+            parts.price, 
+            parts.exchange_date, 
+            parts.kilometers_status, 
+            parts.next_exchange_km,
+            (parts.next_exchange_km + parts.kilometers_status  - cars.mileage) AS when_exchange
+        FROM parts 
+        JOIN cars ON parts.car_id = cars.id
+        WHERE parts.car_id = $car_id AND parts.is_replaced = 0
+        ORDER BY when_exchange ASC
+    ";
+  
+    
+    $result = $conn->query($query);
+    
+    if ($result->num_rows > 0) {
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+      
+      
+      // Sprawdź, czy którakolwiek część ma when_exchange < 0
+            $requires_service = false;
+            foreach ($data as $part) {
+                if ($part['when_exchange'] < 0) {
+                    $requires_service = true;
+                    break;
+                }
+            }
+
+            // Ustaw service_flag = 1 jeśli potrzebna jest wymiana
+            if (!$requires_service) {
+                $updateQuery = "UPDATE cars_info SET service_flag = 0 WHERE car_id = $car_id";
+                $conn->query($updateQuery);
+            }
+      
+      
+      
+        //echo json_encode(['status' => 'success', 'data' => $data]);
+      // } else {
+        //echo json_encode(['status' => 'error', 'message' => 'Brak danych o samochodzie']);
+    }
+    exit();
+
 }
 
 ?>
